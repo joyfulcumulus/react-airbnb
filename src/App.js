eaada5f66c220148b92395813e93a776fc001d6e
YY T <changiyy07@gmail.com>
2024-05-22 16:24:51 +0800

    Mock API call by fetching JSON from Le Wagon Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Flat from './components/flat';
 import styles from './App.module.css';
 
 function App() {
-  const flat = {
-    "id": 148,
-    "name": "Trendy Apt in Buttes Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-    "price": 200,
-    "priceCurrency": "EUR",
-    "lat": 48.885707,
-    "lng": 2.343543
-  };
+  const [flats, setFlats] = useState([]);
 
-  const flats = [flat, flat, flat, flat];
+  useEffect( () => {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setFlats(data);
+      })
+  }, [])
 
   return (
     <div className={styles.app}>
       <div className={styles.main}>
         <div className={styles.search}>Search Box</div>
         <div className={styles.flats}>
-          {flats.map(flat => <Flat flat={flat} />)}
+          {flats.map(flat => <Flat key={flat.id} flat={flat} />)}
         </div>
       </div>
       <div className={styles.map}>
